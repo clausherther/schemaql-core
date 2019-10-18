578c1e89b9ab65176c7223b15a1accf204adc543
Claus Herther <claus@calogica.com>
2019-10-18 15:09:24 -0700

    Adds output json and better folder mgmt

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+import json
 import plac
 from jinja2 import Template, FileSystemLoader, Environment
 from sqlalchemy.inspection import inspect
@@ -7,12 +9,14 @@ import helper
 from connection import SnowflakeConnection
 from logger import logger
 
+
 def make_schema_yaml(schema, table, columns):
     loader = FileSystemLoader("templates/yaml")
     env = Environment(loader=loader)
     template = env.get_template("schema.yml")
     yml = template.render(schema=schema, table=table, columns=columns)
     return yml
+
 
 def get_test_sql(test_name, schema, table, column):
     loader = FileSystemLoader("templates/tests")
@@ -22,66 +26,107 @@ def get_test_sql(test_name, schema, table, column):
     return sql
 
 
-def generate_table_schema(conn, schemas):
+def generate_table_schema(conn, schemas, project_name):
 
     inspector = inspect(conn.engine)
-
+    
     for schema in schemas:
         tables = inspector.get_table_names(schema)
         for table in tables:
             columns = inspector.get_columns(table, schema)
             yml = make_schema_yaml(schema, table, columns)
 
-            with open(f"output/{table}.yml", 'w') as f:
+            schema_directory =  f"output/{project_name}/{schema}"
+            try:
+                os.makedirs(schema_directory)
+            except FileExistsError:
+                # directory already exists
+                pass
+            
+            with open(f"{schema_directory}/{table}.yml", 'w') as f:
                 f.write(yml)
 
-def test_schema(conn, schemas):
+
+def test_schema(conn, schemas, project_name):
+
+    test_results = {}
+    test_results[project_name] = {}
 
     with conn.connect() as cur:
-        p = Path("output")
-        for schema_name in schemas:
-            schema_file_paths = list(p.glob(f"{schema_name}/*.yml"))
 
-            for p in schema_file_paths:
+        for schema_name in schemas:
+            
+            p = Path("output")
+            schema_path = f"{project_name}/{schema_name}/*.yml"
+            schema_files = sorted(list(p.glob(schema_path)))
+            logger.info(f"{schema_name} {schema_path} {schema_files}")
+
+            for p in schema_files:
 
                 table_schema = helper.read_yaml(p.resolve())
 
                 for table in table_schema["models"]:
+
                     table_name = table["name"]
+
                     for column in table["columns"]:
+
                         column_name = column["name"]
+
                         for test_name in column["tests"]:
+
                             sql = get_test_sql(test_name, schema_name, table_name, column_name)
                             rs = cur.execute(sql)
                             result = rs.fetchone()
-                            if result["test_result"] > 0:
-                                logger.info(f'{schema_name}_{table_name}_{column_name}_{test_name}: {result["test_result"]}')
+                            test_result = result["test_result"]
+                            if test_result > 0:
+                                logger.info(f"{schema_name}_{table_name}_{column_name}_{test_name}: {test_result}")
+                                if schema_name not in test_results[project_name]:
+                                    test_results[project_name][schema_name] = {}
+                                if table_name not in test_results[project_name][schema_name]:
+                                    test_results[project_name][schema_name][table_name] = {}
+                                if column_name not in test_results[project_name][schema_name][table_name]:
+                                    test_results[project_name][schema_name][table_name][column_name] = {}
 
-def main(action: ("Action ('test', or 'generate')", "option", "a") = "test",
-         project_file: ("Project file", "option", "p") = "projects.yml",
-         connections_file: ("Connections file", "option", "c") = "connections.yml"):
+                                test_results[project_name][schema_name][table_name][column_name] = {test_name: test_result}
 
-    projects = helper.read_yaml(project_file)["projects"]
+    return test_results
 
-    connections = helper.read_yaml(connections_file)
 
-    for project in projects:
+def main(action_prm: ("Action ('test', or 'generate')", "option", "a") = "dry",
+         project_prm: ("Project", "option", "p") = "",
+         project_file_prm: ("Project file", "option", "f") = "projects.yml",
+         connections_file_prm: ("Connections file", "option", "c") = "connections.yml"):
 
+    projects = helper.read_yaml(project_file_prm)["projects"]
+    connections = helper.read_yaml(connections_file_prm)
+
+    for project_name in projects:
+        if len(project_prm) > 0 and project_prm != project_name:
+            continue
+
+        logger.info(project_name)
+        project = projects[project_name]
         connection_name = project["connection"]
-        logger.info(connection_name)
+        logger.info(f"{project_name} {connection_name}")
 
         connection_info = connections[connection_name]
         connection_types = {"snowflake": SnowflakeConnection}
         connection_type = connection_info["type"]
+        if connection_type not in connection_types:
+            raise NotImplementedError(f"Connection '{connection_type}' is not yet implemented")
 
         schemas = project["schema"]
-
+        logger.info(schemas)
         conn = connection_types[connection_type](connection_info)
 
-        if action == "generate":
-            generate_table_schema(conn, schemas)
-        elif action == "test":
-            test_schema(conn, schemas)
+        if action_prm == "generate":
+            generate_table_schema(conn, schemas, project_name)
+        elif action_prm == "test":
+            test_results = test_schema(conn, schemas, project_name)
+            test_results_json = json.dumps(test_results, indent=4, sort_keys=True)
+            with open(f"output/{project_name}/test_results.json", 'w') as f:
+                f.write(test_results_json)
 
 if __name__ == '__main__':
 
