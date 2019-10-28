ed5e01f4f8a87e62dfd84038cce8c04453f7a831
Claus Herther <claus@calogica.com>
2019-10-28 16:09:26 -0700

    Adds accepted values test

--- schemaql/templates/tests/accepted_values.sql
+++ schemaql/templates/tests/accepted_values.sql
@@ -1,8 +1,27 @@
+{% set quote_values = kwargs.get('quote', True) %}
+
+with all_values as (
+    select distinct
+        {{ column }} as value_field
+    from {{ table }}
+),
+validation_errors as (
+
+    select
+        value_field
+
+    from all_values
+    where value_field not in (
+        {% for val in kwargs["values"] -%}
+            {% if quote_values -%}
+            '{{ val }}'
+            {%- else -%}
+            {{ val }}
+            {%- endif -%}
+            {%- if not loop.last -%},{%- endif %}
+        {%- endfor %}
+    )
+)
+
 select count(*) as test_result
-from {{ table }}
-where
-    {{ column }} not in (
-        {%- for val in kwargs["values"] -%}
-        '{{ val }}'{% if not loop.last %},{% endif %}
-        {% endfor -%}
-        )
+from validation_errors
