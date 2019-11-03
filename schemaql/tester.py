6beaac5611171d8b269a6e0f7a5095229c5d5ce5
Claus Herther <claus@calogica.com>
2019-11-03 08:30:27 -0800

    Adds tester

--- schemaql/tester.py
+++ schemaql/tester.py
@@ -7,86 +7,102 @@ from schemaql.helper import check_directory_exists, read_yaml, schemaql_path
 from schemaql.logger import logger, Fore, Back, Style
 
 
-def log_test_result(schema_name, table_name, column_name, test_name, test_result):
 
-    LINE_WIDTH = 88
-    RESULT_WIDTH = 30
-    MSG_WIDTH = LINE_WIDTH - RESULT_WIDTH  # =58
+class TableTester(object):
+    """
+    Schema Generator
+    """
 
-    result_msg = f"{table_name}.{column_name}: {test_name}"[:MSG_WIDTH]
-    result_msg = result_msg.ljust(MSG_WIDTH, ".")
+    def __init__(self, project_name, connector, database_name, schema_name, table_name):
 
-    if test_result == 0:
-        colored_pass = Fore.GREEN + "PASS" + Style.RESET_ALL
-        logger.info(result_msg + f"[{colored_pass}]".rjust(RESULT_WIDTH, "."))
+        self._project_name = project_name
+        self._connector = connector
+        self._database_name = database_name
+        self._schema_name = schema_name
+        self._table_name = table_name
+        
+        self._template_path = schemaql_path.joinpath("templates", "tests").resolve()
+        self._loader = FileSystemLoader(str(self._template_path))
+        self._env = Environment(loader=self._loader)
 
-    else:
-        colored_fail = Fore.RED + f"FAIL {test_result:,}" + Style.RESET_ALL
-        logger.error(result_msg + f"[{colored_fail}]".rjust(RESULT_WIDTH, "."))
-
-
-def update_test_results(
-    project_name,
-    schema_name,
-    table_name,
-    column_name,
-    test_name,
-    test_results,
-    test_result,
-):
-
-    if test_result > 0:
-        if schema_name not in test_results[project_name]:
-            test_results[project_name][schema_name] = {}
-
-        if table_name not in test_results[project_name][schema_name]:
-            test_results[project_name][schema_name][table_name] = {}
-
-        if column_name not in test_results[project_name][schema_name][table_name]:
-            test_results[project_name][schema_name][table_name][column_name] = {}
-
-        test_results[project_name][schema_name][table_name][column_name] = {
-            test_name: test_result
-        }
-
-    return test_results
+    def get_test_sql(self, test_name, column_name, kwargs=None):
+        template = self._env.get_template(f"{test_name}.sql")
+        sql = template.render(
+            # database=self._database_name, 
+            schema=self._schema_name, 
+            table=self._table_name, 
+            column=column_name, 
+            kwargs=kwargs
+        )
+        return sql
 
 
-def get_test_sql(test_name, database, schema, table, column, kwargs=None):
-    template_path = schemaql_path.joinpath("templates", "tests").resolve()
-    loader = FileSystemLoader(str(template_path))
-    env = Environment(loader=loader)
-    template = env.get_template(f"{test_name}.sql")
-    sql = template.render(
-        database=database, schema=schema, table=table, column=column, kwargs=kwargs
-    )
-    return sql
+    def log_test_result(self, column_name, test_name, test_result):
+
+        LINE_WIDTH = 88
+        RESULT_WIDTH = 30
+        MSG_WIDTH = LINE_WIDTH - RESULT_WIDTH  # =58
+
+        result_msg = f"{self._table_name}.{column_name}: {test_name}"[:MSG_WIDTH]
+        result_msg = result_msg.ljust(MSG_WIDTH, ".")
+
+        if test_result == 0:
+            colored_pass = Fore.GREEN + "PASS" + Style.RESET_ALL
+            logger.info(result_msg + f"[{colored_pass}]".rjust(RESULT_WIDTH, "."))
+
+        else:
+            colored_fail = Fore.RED + f"FAIL {test_result:,}" + Style.RESET_ALL
+            logger.error(result_msg + f"[{colored_fail}]".rjust(RESULT_WIDTH, "."))
 
 
-def test_schema(conn, databases, project_name):
+    def update_test_results(
+        self,
+        column_name,
+        test_name,
+        test_results,
+        test_result,
+    ):
+
+        if test_result > 0:
+            if self._schema_name not in test_results[self._project_name]:
+                test_results[self._project_name][self._schema_name] = {}
+
+            if self._table_name not in test_results[self._project_name][self._schema_name]:
+                test_results[self._project_name][self._schema_name][self._table_name] = {}
+
+            if column_name not in test_results[self._project_name][self._schema_name][self._table_name]:
+                test_results[self._project_name][self._schema_name][self._table_name][column_name] = {}
+
+            test_results[self._project_name][self._schema_name][self._table_name][column_name] = {
+                test_name: test_result
+            }
+
+        return test_results
+
+
+
+def test_schema(connector, databases, project_name):
 
     test_results = {}
     test_results[project_name] = {}
 
-    for database in databases:
-        conn.database = database
-        conn.make_engine()
+    for database_name in databases:
+        connector.database = database_name
 
-        logger.info(f"Inspecting database {database}...")
-        inspector = inspect(conn.engine)
+        logger.info(f"Inspecting database {database_name}...")
 
-        schemas = databases[database]
+        schemas = databases[database_name]
 
         if schemas is None:
             logger.info("No schemas specified, getting all schemas from database...")
-            schemas = sorted(inspector.get_schema_names())
+            schemas = connector.get_schema_names()
 
         for schema_name in schemas:
 
-            with conn.connect() as cur:
+            with connector.connect() as cur:
 
                 p = Path("output")
-                schema_path = Path(project_name).joinpath(database, schema_name, "*.yml")
+                schema_path = Path(project_name).joinpath(database_name, schema_name, "*.yml")
                 schema_files = sorted(list(p.glob(str(schema_path))))
 
                 for p in schema_files:
@@ -96,7 +112,7 @@ def test_schema(conn, databases, project_name):
                     for table in table_schema["models"]:
 
                         table_name = table["name"]
-
+                        table_tester = TableTester(project_name, connector, database_name, schema_name, table_name)
                         for column in table["columns"]:
 
                             column_name = column["name"]
@@ -109,11 +125,8 @@ def test_schema(conn, databases, project_name):
                                 else:
                                     test_name = test
 
-                                sql = get_test_sql(
+                                sql = table_tester.get_test_sql(
                                     test_name,
-                                    database,
-                                    schema_name,
-                                    table_name,
                                     column_name,
                                     kwargs,
                                 )
@@ -122,18 +135,13 @@ def test_schema(conn, databases, project_name):
                                 result = rs.fetchone()
                                 test_result = result["test_result"]
 
-                                log_test_result(
-                                    schema_name,
-                                    table_name,
+                                table_tester.log_test_result(
                                     column_name,
                                     test_name,
                                     test_result,
                                 )
 
-                                test_results = update_test_results(
-                                    project_name,
-                                    schema_name,
-                                    table_name,
+                                test_results = table_tester.update_test_results(
                                     column_name,
                                     test_name,
                                     test_results,
