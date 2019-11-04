d985897e5cbff653a284b9fb1f5ed19cf7f451b6
Claus Herther <claus@calogica.com>
2019-11-03 17:25:45 -0800

    Adds support for roles and warehouses

--- schemaql/connectors/snowflake.py
+++ schemaql/connectors/snowflake.py
@@ -1,8 +1,8 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 
-# from snowflake.sqlalchemy import URL
-# import snowflake.connector
+from snowflake.sqlalchemy import URL
+import snowflake.connector
 
 from schemaql.connectors.base_connector import Connector
 
@@ -15,6 +15,14 @@ class SnowflakeConnector(Connector):
     def __init__(self, connection_info):
 
         self._account = connection_info["account"]
+        self._conn_params = {}
+
+        _prms = ["warehouse", "role"]
+
+        for p in _prms:
+            if p in connection_info:
+                self._conn_params[p] = connection_info[p]
+
         super().__init__(connection_info)
 
     @property
@@ -25,6 +33,23 @@ class SnowflakeConnector(Connector):
     def account(self, val):
         self._account = val
 
+    @property
+    def warehouse(self):
+        return self._warehouse
+
+    @warehouse.setter
+    def warehouse(self, val):
+        self._warehouse = val
+
+    @property
+    def role(self):
+        return self._role
+
+    @role.setter
+    def role(self, val):
+        self._role = val
+
+
     def _make_url(self):
 
         url = f"snowflake://{self._user}:{self._password}@{self._account}"
@@ -32,5 +57,10 @@ class SnowflakeConnector(Connector):
             url += f"/{self.database}"
         if self.schema:
             url += f"/{self.schema}"
+
+        if self._conn_params:
+            url += f"?"
+            for k,v in self._conn_params.items():
+                url += f"{k}={v}&"
 
         return url
