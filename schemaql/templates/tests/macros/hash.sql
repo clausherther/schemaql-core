76aecfbb426555e6e4712b562c4a533da07b1933
Claus Herther <claus@calogica.com>
2019-11-06 09:43:27 -0800

    Moves concat into hash macro

--- schemaql/templates/tests/macros/hash.sql
+++ schemaql/templates/tests/macros/hash.sql
@@ -1,9 +1,17 @@
+{%- macro row_key(columns) -%}
+    concat(
+    {% for col in columns -%}
+        coalesce(cast({{ col }} as {{ type_string() }}), '')
+        {%- if not loop.last %},"|",{% endif -%}
+    {% endfor -%}
+    )
+{%- endmacro -%}
 {% macro hash(columns) -%}
    {{ connector_macro('hash', columns) }}
 {%- endmacro %}
 
 {% macro default__hash(columns) -%}
-    md5(cast({{columns}} as {{ type_string() }} ))
+    md5({{ row_key(columns) }})
 {%- endmacro %}
 
 {% macro bigquery__hash(columns) -%}
--- schemaql/templates/tests/unique_rows.sql
+++ schemaql/templates/tests/unique_rows.sql
@@ -1,16 +1,15 @@
-{% macro row_key(columns) %}
-    concat(
-    {% for col in columns -%}
-        cast({{ col }} as {{ type_string() }}){% if not loop.last %},{% endif %}
-    {% endfor -%}
-    )
-{% endmacro %}
-
-{% set columns = kwargs["columns"] %}
-
+{%- if kwargs and "columns" in kwargs %}
+{%- set columns = kwargs["columns"] -%}
+{%- else -%}
+{%- set columns = connector.get_column_names(table, schema) -%}
+{%- endif -%}
+{%- if kwargs and "except" in kwargs %}
+{%- set except_columns = kwargs["except"] -%}
+{%- set columns = columns | difference(except_columns) -%}
+{%- endif -%}
 with hashed_rows as (
     select 
-        {{ hash(row_key(columns)) }} as row_hash
+        {{ hash(columns) }} as row_hash
      from {{ schema }}.{{ table }}
 )
 select (count(*) - count(distinct row_hash)) as test_result
