4f7abf6bde647b192f98edf5d7450459e6bce312
Claus Herther <claus@calogica.com>
2019-11-20 14:52:03 -0800

    Updates setup files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 package_name = "schemaql"
 package_version = "0.0.4"
-description = "A testing and auditing tool inspired by dbt, for those not using dbt."
+description = "A data quality and auditing tool inspired by dbt test"
 
 setup(
     name=package_name,
@@ -17,22 +17,40 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     # download_url
-    # url="https://github.com/clausherther/schemaql",
+    url="https://schemaql.com",
     packages=find_packages(),
-    data_files=[('include', ['*.sql', '*.yml'])],
+    include_package_data=True,
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.sql', '*.yml', '*.md', '*.html'],
+    },
     scripts=['bin/schemaql'],
     python_requires='>=3.6',
     install_requires=[
+        'colorama==0.3.9',
         'cffi==1.13.0',
         'Jinja2>=2.10',
-        'PyYAML>=5.1',
+        'pandas>=0.25.3',
         'plac>=1.0.0',
+        'psycopg2>=2.7.5,<2.8',
         'pybigquery>=0.4.11',
-        'colorama==0.3.9',
+        'PyYAML>=5.1',
         'snowflake-connector-python==2.0.3',
         'snowflake-sqlalchemy>=1.1.14',
         'SQLAlchemy>=1.3.7',
         'sqlalchemy-redshift>=0.7.5',
-        'psycopg2>=2.7.5,<2.8',
-    ]
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
